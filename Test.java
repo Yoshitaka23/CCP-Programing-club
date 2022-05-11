20837f112bf38635f7f3d68cabda46e6a23a2a80
Yoshitaka23 <105221213+Yoshitaka23@users.noreply.github.com>
2022-05-11 14:03:32 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,4 +2,15 @@ class main{
   public Static void main(String[] args){
     System.out.println("Test")
   }
+  
+  public int checkAve(int[][] x){
+    int sum = 0;
+    for(int[] nums: x){
+      for(int num: nums){
+        sum += num;
+      }
+    }
+    return sum;
+  }
+  
 }
